656629c9e219bbe15975c6a6c4d93fc817c68c27
Yaswanth Varala <yaswanthvarala7336@gmail.com>
2025-12-22 07:46:38 +0000

    my second comit

---
+++ macros/cents_to_dollars.sql
@@ -0,0 +1,3 @@
+{%- macro cents_to_dollars(column_name, decimal_places=2) -%}
+{{ column_name }} / 100
+{%- endmacro -%}
--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -7,7 +7,7 @@ with customers as (
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{source('jaffle_shop','customers')}}
 
 )
 
