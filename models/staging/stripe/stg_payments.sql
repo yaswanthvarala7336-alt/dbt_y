51f0dd914c80db69c17d0cd7ab753193ca3172db
Yaswanth Varala <yaswanthvarala7336@gmail.com>
2025-12-23 04:11:37 +0000

    my fourth comit

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,10 +1,8 @@
 select
-
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    {{ cents_to_dollars('amount') }} as amount,
+    amount / 100 as amount, -- Converts amount from cents to dollars
     created as created_at
-
-from {{source('strip','payments')}}
+from {{ source('stripe', 'payments') }}
