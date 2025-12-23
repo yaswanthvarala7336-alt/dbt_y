f669302a35e525af640be104fbd25391e8222acd
Yaswanth Varala <yaswanthvarala7336@gmail.com>
2025-12-23 04:32:02 +0000

    this is my final commit

--- macros/cents_to_dollars.sql
+++ macros/cents_to_dollars.sql
@@ -1,3 +1,3 @@
-{% macro cents_to_dollars(column) %}
-  {{ column }} / 100
-{% endmacro %}
+{%- macro cents_to_dollars(column_name , decimal_places = 2) -%}
+{{ column_name }} / 100
+{%- endmacro -%}
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,6 +3,6 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    amount / 100 as amount, -- Converts amount from cents to dollars
+    {{ cents_to_dollars('amount') }} as amount, -- Converts amount from cents to dollars
     created as created_at
 from {{ source('stripe', 'payments') }}
